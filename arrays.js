365f951892c6ea1bbd5016544ddb81ba93fe6bbf
noah.stewart9813@gmail.com <noah.stewart9813@gmail.com>
2018-08-06 19:37:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,4 +31,7 @@ function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
 }
 
-function 
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
